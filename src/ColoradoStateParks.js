190393ca7c2a2bd50e7d0d1507381c2c297e8c39
Rawllings Kiprop <rutojr01@gmail.com>
2023-01-11 12:31:06 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import HowManyParks from "./parks/HowManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  HowManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <> 
+     <h1>Colorado State Parks!</h1>
+     <MesaVerde />
+     {trees}
+
+    </>
+
+  )
+
 }
+
+export default ColoradoStateParks
